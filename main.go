804740833698d4c6dd93097e74fcb2ac87ce8f88
Martin Goldstone <martin.goldstone@gmail.com>
2022-03-01 23:25:27 +0000

    Add caching of alpha vantage response

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/martingoldstone/stockticker/alphavantage"
+	"github.com/martingoldstone/stockticker/stocktickerresponse"
 )
 
 var (
-	Symbol string
-	Days   int
-	Key    string
-	SDays  string
+	Symbol    string
+	Days      int
+	Key       string
+	SDays     string
+	stckr     *stocktickerresponse.StockTickerResponse
+	cachetime time.Time
+	mu        sync.Mutex
 )
 
 func getFavicon(w http.ResponseWriter, req *http.Request) {
@@ -25,39 +31,59 @@ func getFavicon(w http.ResponseWriter, req *http.Request) {
 	return
 }
 
+func timenow() string {
+	return time.Now().Format(time.RFC3339)
+}
+
+func updateCache(s *stocktickerresponse.StockTickerResponse) {
+	//Lock updating the cache variable in case multiple requests try to run before cache is initialised or after cache has expired
+	mu.Lock()
+	defer mu.Unlock()
+	stckr = s
+	cachetime = time.Now()
+}
+
 func getStockData(w http.ResponseWriter, req *http.Request) {
-	log.Printf("%s %s %s Processing request...\n", req.Method, req.RequestURI, req.RemoteAddr)
+	log.Printf("%s %s %s Processing request...\n", req.RemoteAddr, req.Method, req.RequestURI)
 
-	h, err := http.Get(fmt.Sprintf("https://www.alphavantage.co/query?apikey=%s&function=TIME_SERIES_DAILY&symbol=%s", Key, Symbol))
-	if err != nil {
-		log.Printf("Failed connecting to alpha vantage: %v\n", err)
-		http.Error(w, "Failed opening connection to Alpha Vantage", http.StatusInternalServerError)
-		return
-	}
+	//Data is refreshed at end of trading time (16:00 US/Eastern), but fluctuations observed after end of trading so only cache for 1 hour
+	//Alpha Vantage quotas requests per API key, caching necessary to avoid running out of quota
+	if stckr == nil || time.Now().After(cachetime.Add(1*time.Hour)) {
 
-	defer h.Body.Close()
-
-	if h.StatusCode != 200 {
-		etxt, e := ioutil.ReadAll(h.Body)
-		if e != nil {
-			etxt = []byte("Could not decode response body")
+		h, err := http.Get(fmt.Sprintf("https://www.alphavantage.co/query?apikey=%s&function=TIME_SERIES_DAILY&symbol=%s", Key, Symbol))
+		if err != nil {
+			log.Printf("Failed connecting to alpha vantage: %v\n", err)
+			http.Error(w, "Failed opening connection to Alpha Vantage", http.StatusInternalServerError)
+			return
 		}
-		http.Error(w, string(etxt), h.StatusCode)
-		return
+
+		defer h.Body.Close()
+
+		if h.StatusCode != 200 {
+			etxt, e := ioutil.ReadAll(h.Body)
+			if e != nil {
+				etxt = []byte("Could not decode response body")
+			}
+			http.Error(w, string(etxt), h.StatusCode)
+			return
+		}
+
+		resp, err := alphavantage.ParseAlphaVantage(h.Body)
+		if err != nil {
+			http.Error(w, "Failed to decode API response from Alpha Vantage", http.StatusInternalServerError)
+			return
+		}
+		updateCache(resp.ToStockTickerResponse(Days))
+		if stckr == nil {
+			http.Error(w, "Failed to transcode API response from Alpha Vantage into response structure", http.StatusInternalServerError)
+			return
+		}
+
+	} else {
+		log.Printf("%s %s %s Serving from cache\n", req.RemoteAddr, req.Method, req.RequestURI)
 	}
 
-	resp, err := alphavantage.ParseAlphaVantage(h.Body)
-	if err != nil {
-		http.Error(w, "Failed to decode API response from Alpha Vantage", http.StatusInternalServerError)
-		return
-	}
-	str := resp.ToStockTickerResponse(Days)
-	if str == nil {
-		http.Error(w, "Failed to transcode API response from Alpha Vantage into response structure", http.StatusInternalServerError)
-		return
-	}
-
-	rj, err := json.MarshalIndent(str, "", "    ")
+	rj, err := json.MarshalIndent(stckr, "", "    ")
 	if err != nil {
 		http.Error(w, "Could not render response structure to JSON", http.StatusInternalServerError)
 		return
